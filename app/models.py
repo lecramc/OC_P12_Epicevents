3cafceac0c35dbd5dc0594f6a023f6cb1589e4f4
Clement MARCEL <clmtmarcel@gmail.com>
2023-01-14 16:06:29 +0100

    create app and models

---
+++ app/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+class ClientType(models.Model):
+    title = models.CharField(max_length=30)
+
+class EventStatus(models.Model):
+    type = models.CharField(max_length=30)
+    
+class ContactType(models.Model):
+    title = models.CharField(max_length=30)
+
+class Contact(models.Model):
+    firstname =models.CharField(max_length=25)
+    lastname = models.CharField(max_length=25)
+    type = models.ForeignKey(to=ContactType, on_delete=models.CASCADE)
+
+class Client(models.Model):
+    firstname = models.CharField(max_length=25)
+    lastname = models.CharField(max_length=25)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    mobile = models.CharField(max_length=20)
+    company = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now_add=True)
+    sales_contact = models.ForeignKey(to=Contact, on_delete=models.CASCADE)
+    type = models.ForeignKey(to=ClientType, on_delete=models.CASCADE)
+
+class Contract(models.Model):
+    sales_contact = models.ForeignKey(to=Contact, on_delete=models.CASCADE)
+    client = models.ForeignKey(to=Client, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField()
+    amount = models.FloatField()
+    payment_due = models.DateTimeField(auto_now_add=True)
+
+class Event(models.Model):
+    client = models.ForeignKey(to=Client, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now_add=True)
+    support_contact = models.ForeignKey(to=Contact, on_delete=models.CASCADE)
+    event_status = models.ForeignKey(to=EventStatus, on_delete=models.CASCADE)
+    attendees = models.IntegerField()
+    event_date = models.DateTimeField(auto_now_add=True)
+    notes = models.TextField()
+
