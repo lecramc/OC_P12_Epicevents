53144d3688f4490814cc869e9d6092d01da9d834
Clement MARCEL <clmtmarcel@gmail.com>
2023-01-27 14:47:19 +0100

    change contact model to user

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class EventStatus(models.Model):
 class ContactType(models.Model):
     title = models.CharField(max_length=30)
 
-class Contact(models.Model):
+class User(models.Model):
     firstname =models.CharField(max_length=25)
     lastname = models.CharField(max_length=25)
     type = models.ForeignKey(to=ContactType, on_delete=models.CASCADE)
@@ -23,11 +23,11 @@ class Client(models.Model):
     company = models.CharField(max_length=100)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now_add=True)
-    sales_contact = models.ForeignKey(to=Contact, on_delete=models.CASCADE)
+    sales_contact = models.ForeignKey(to=User, on_delete=models.CASCADE)
     type = models.ForeignKey(to=ClientType, on_delete=models.CASCADE)
 
 class Contract(models.Model):
-    sales_contact = models.ForeignKey(to=Contact, on_delete=models.CASCADE)
+    sales_contact = models.ForeignKey(to=User, on_delete=models.CASCADE)
     client = models.ForeignKey(to=Client, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now_add=True)
@@ -39,7 +39,7 @@ class Event(models.Model):
     client = models.ForeignKey(to=Client, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now_add=True)
-    support_contact = models.ForeignKey(to=Contact, on_delete=models.CASCADE)
+    support_contact = models.ForeignKey(to=User, on_delete=models.CASCADE)
     event_status = models.ForeignKey(to=EventStatus, on_delete=models.CASCADE)
     attendees = models.IntegerField()
     event_date = models.DateTimeField(auto_now_add=True)
